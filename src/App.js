3a1b93a794f84449076d4e8c8514ac43544adde2
anaswins350-ai <anaswins35@gmail.com>
2026-05-02 05:20:51 +0100

    refactor: update App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './styles/App.css'
 import Sidebar from './components/layout/Sidebar/Sidebar'
-
-
+import Overview from './pages/Overview/Overview';
+import CurrentWeather from './pages/CurrentWeather/CurrentWeather';
+import Forecast from './pages/Forecast/Forecast';
+import Map from './pages/Map/Map';
+import Settings from './pages/Settings/Settings';
+import PageNotFound from "./pages/PageNotFound/PageNotFound"
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Overview />} />
+            <Route path="/current-weather" element={<CurrentWeather />} />
+            <Route path="/forecast" element={<Forecast />} />
+            <Route path="/map" element={<Map />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
